7227a352196bd56a5542e08d2fa5a2002f5cef26
TaeYai <TaeYai123@gmail.com>
2021-08-20 23:43:28 +0700

    icon

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -26,9 +26,9 @@ class HealthIcon extends FlxSprite
 		animation.add('mom-car', [6, 7], 0, false, isPlayer);
 		animation.add('tankman', [8, 9], 0, false, isPlayer);
 		animation.add('face', [10, 11], 0, false, isPlayer);
-		animation.add('star', [10, 11], 0, false, isPlayer);
-		animation.add('squid', [10, 11], 0, false, isPlayer);
-		animation.add('sponge', [10, 11], 0, false, isPlayer);
+		animation.add('star', [26, 27], 0, false, isPlayer);
+		animation.add('squid', [28, 29], 0, false, isPlayer);
+		animation.add('sponge', [24, 25], 0, false, isPlayer);
 		animation.add('dad', [12, 13], 0, false, isPlayer);
 		animation.add('senpai', [22, 22], 0, false, isPlayer);
 		animation.add('senpai-angry', [22, 22], 0, false, isPlayer);
